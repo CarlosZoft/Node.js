2351c7a802acc93a41c0cd4c350342a70ddc48a4
Carlos <carlosoftwar@gmail.com>
2021-01-12 18:40:04 -0300

    public folder : Bootstrap

--- formValidation/index.js
+++ formValidation/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+var session = require('express-session');
+var flash = require('express-flash');
 
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+app.use(flash());
+
+app.get("/", (req,res)=>{
+    res.json("Aplicação está funcionando!")
+})
 
 app.listen(3000 ,(req,res) => {
     console.log("App running");
