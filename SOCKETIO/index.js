423b87fb7f73e6eff533d872b9c8cd69dc0604c2
Carlos <carlosoftwar@gmail.com>
2021-01-13 20:11:22 -0300

    connect and disconnect socketio

--- SOCKETIO/index.js
+++ SOCKETIO/index.js
@@ -8,3 +8,29 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) =>{
     res.render("index")
 })
+
+io.on("connection",(client) => {
+
+    client.on("disconnect", () =>{
+        console.log("X desconectou" + client.id)
+    })
+
+    client.on("boasvindas", (data)=>{
+        console.log(data);
+    })
+
+    client.on("palavras", data =>{
+        client.emit("resultado", data + " Melhor do mundo!");
+    })
+})
+
+
+
+
+
+
+
+
+http.listen(3678, () => {
+    console.log("app running");
+})
